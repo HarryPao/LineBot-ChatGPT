b64570c1b23a32986baf6a2890faad8b6bfbd67d
Harry Pao <harrybou2009@gmail.com>
2023-12-27 11:45:09 +0800

    feat: change the mechanism of data storage
    1. transfer data storage mechanism from JSON(userInfo.json) to Database(PostgreSQL).

--- app.py
+++ app.py
@@ -1,23 +1,35 @@
+# Standard Library Imports
 import os
-from flask import Flask, request
-from linebot import LineBotApi
-from linebot.v3.webhook import WebhookHandler
-from linebot.models import TextSendMessage
 import json
 import threading
 import time
 from datetime import datetime, timedelta
+
+# Third-Party Imports
+from flask import Flask, request
+from linebot import LineBotApi
+from linebot.v3.webhook import WebhookHandler
+from linebot.models import TextSendMessage
 import schedule
 import requests
 
+# Local Imports
+from db_module.db_operations import PostgreSQLHandler
+
 app = Flask(__name__)
-json_file_path = './tmp/userInfo.json'
 
 # Initialize LineBot API
 line_bot_api= LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 
-# Global lock to synchronize file access
-file_lock = threading.Lock()
+# Initialize the PostgreSQLHandler
+db_handler = PostgreSQLHandler(
+    dbname='linebot',
+    user='postgres',
+    password='1234',
+    host='localhost',
+    port='5432' # Default PostgreSQL port
+)
+
 
 @app.route("/", methods=['POST'])
 def linebot():
@@ -43,39 +55,38 @@ def linebot():
         user_id = json_data['events'][0]['source']['userId']
         reply_token = json_data['events'][0]['replyToken']
         user_message = json_data['events'][0]['message']['text']
-       # Use user_id to get the profile of the user
+        # Use user_id to get the profile of the user
         profile = line_bot_api.get_profile(user_id)
         user_name = profile.display_name
 
-        with file_lock:
-            # Check if the message starts with 'hi ai:, if it does, enter AI mode.
-            if user_message[:5].lower() == 'hi ai' or checkUserModeStatus(user_id):
+        # Check if the message starts with 'hi ai:, if it does, enter AI mode.
+        if user_message[:5].lower() == 'hi ai' or checkUserModeStatus(user_id):
 
-                # Check if the user have enough quota to ask question
-                if checkUserMsgQuota(user_id, user_name):
+            # Check if the user have enough quota to ask question
+            if checkUserMsgQuota(user_id, user_name):
 
-                    # Enter AI mode if the message starts with 'hi ai'.(Assuming that the user use 'hi ai' to enter AI mode)
-                    if user_message[:5].lower() == 'hi ai':
-                        enterAImode(user_id)
+                # Enter AI mode if the message starts with 'hi ai'.(Assuming that the user use 'hi ai' to enter AI mode)
+                if user_message[:5].lower() == 'hi ai':
+                    enterAImode(user_id)
 
-                    # Record last msg time
-                    updateLastAImsgTime(user_id)
+                # Record last msg time
+                updateLastAImsgTime(user_id)
 
-                    # Redirect the question to chatPDF
-                    reply_msg = askChatPDF(user_message)
+                # Redirect the question to chatPDF
+                reply_msg = askChatPDF(user_message)
 
-                # The user does not have enough quota to ask question
-                else:
-                    reply_msg = "We're sorry, but you've reached the message limit of the day. Please ask again tomorrow."
-
-            # If not a special command, echo the user's message
-            # Use tradtional linebot mode
+            # The user does not have enough quota to ask question
             else:
-                reply_msg = user_message
-            
-            # Send the reply message back to the user
-            text_message = TextSendMessage(text=reply_msg)
-            line_bot_api.reply_message(reply_token,text_message)
+                reply_msg = "We're sorry, but you've reached the message limit of the day. Please ask again tomorrow."
+
+        # If not a special command, echo the user's message
+        # Use tradtional linebot mode
+        else:
+            reply_msg = user_message
+        
+        # Send the reply message back to the user
+        text_message = TextSendMessage(text=reply_msg)
+        line_bot_api.reply_message(reply_token,text_message)
 
     except Exception as e:
         # Print any exceptions for debugging purposes
@@ -89,122 +100,71 @@ def checkUserMsgQuota(user_id, user_name):
     If the user has no quota, return false to reject the user from asking.
     If the user is a new user to this LineBot, add his/her  userId to the userInfo.json, and return ture to enable asking question."""
 
-    # Read the JSON file
-    with open(json_file_path, 'r') as json_file:
+    selected_data = db_handler.select_data('users', condition=f"userid = '{user_id}'")
 
-        # Load JSON data
-        data = json.load(json_file)
-        
-        # Check if this user_id exists and has enough quota of messages to ask
-        for user in data:
-            if user.get("userId") == user_id:
+    if selected_data != []:
+        userName = selected_data[0][1]
+        userQuota = selected_data[0][3]
 
-                # Check if this user has modified his/her profile name(display name); if he/her had, modify in userInfo.json
-                if user['userName'] != user_name:
-                    modifyUserName(data, user, user_name)
+        if userName != user_name:
+            modifyUserName(user_id, user_name)
 
-                if user['quota'] == 0:
-                    return False
-                else:
-                    # User have enough quota, quota-=1
-                    userMsgQuotaDecreaseOne(data, user, user['quota'])
-                    return True
-                
-        # Cannot find this user_id, then it is a new user.
-        # Add this user to the JSON file
-        addUser(data, user_id, user_name)
+        if userQuota == 0:
+            return False
+        else:
+            userMsgQuotaDecreaseOne(selected_data)
+            return True
+    
+    else:
+        # Cannot find this user_id, then it is a new user,
+        # Add this user to the DB table
+        addUser(user_id, user_name)
         return True
 
-def modifyUserName(json_data, user, user_name):
-    """Modify user's profile name (display name) in userInfo.json."""
+def modifyUserName(user_id, user_name):
+    """Modify user's profile name (displayed name) in DB"""
 
-    # Update the old user name to new user name
-    user.update({"userName": user_name})
+    update_condition = f"userid = '{user_id}'"
+    update_data = {'username': user_name}
+    db_handler.update_data('users', update_data, update_condition)
 
-    # Write the updated info to the ./tmp/userInfo.json
-    with open(json_file_path, 'w') as json_file:
-        json.dump(json_data, json_file, indent=4)
-
-    return 'OK'
-
-def addUser(json_data, user_id, user_name):
-    """Add the userId of the new user to the userInfo.json, and set the quota to 49,
+def addUser(user_id, user_name):
+    """Add the new user to DB, and set the quota to 49,
     because the user would use 1 quota upon asking question."""
+
+    data_to_insert = {'userid': user_id, 'username': user_name, 'quota': 49}
+    db_handler.insert_data('users', data_to_insert)
+
+def userMsgQuotaDecreaseOne(select_data):
+    """Decrease user's message quota by 1"""
+    userId = select_data[0][2]
+    userQuota = select_data[0][3]
+    userQuota -= 1
     
-    # Set the info of new_user
-    new_user = {"userName": f"{user_name}", "userId": f"{user_id}", "quota": 49}
-
-    # Append the new user info to the bottom of the userInfo.json
-    json_data.append(new_user)
-    with open(json_file_path, 'w') as json_file:
-        json.dump(json_data, json_file, indent=4)
-
-    return 'OK'
-
-def userMsgQuotaDecreaseOne(json_data, user, quota):
-    """Decrease the quota of message of a specific user."""
-
-    # Update remain quota of the user
-    quota -= 1
-    user.update({"quota": quota})
-
-    # Write the updated info to the ./tmp/userInfo.json
-    with open(json_file_path, 'w') as json_file:
-        json.dump(json_data, json_file, indent=4)
-
-    return 'OK'
+    update_condition = f"userid ='{userId}'"
+    update_data = {'quota': userQuota}
+    db_handler.update_data('users', update_data, update_condition)
 
 def checkUserModeStatus(user_id):
-    """Check userInfo.json to if user is in AI mode."""
+    """See if the user is in AI-mode"""
 
-    # Read the JSON file
-    with open(json_file_path, 'r') as json_file:
-
-        # Load JSON data
-        data = json.load(json_file)
-
-        # Check user's mode status
-        for user in data:
-            if user.get("userId") == user_id:
-                return user['AImode']
+    selected_data = db_handler.select_data('users', condition=f"userid = '{user_id}'")
+    userAImode = selected_data[0][4]
+    return userAImode
 
 def enterAImode(user_id):
-    """Turn user's AImode status into active(true)."""
+    """Turn the user's AImode status into active(true)."""
 
-    # Read the JSON file
-    with open(json_file_path, 'r') as json_file:
-
-        # Load JSON data
-        data = json.load(json_file)
-
-        # Search the user info and update his/her AImode into active(true).
-        for user in data:
-            if user.get("userId") == user_id:
-                user.update({"AImode": True})
-                break
-            
-    # Write the file
-    with open(json_file_path, 'w') as json_file:
-        json.dump(data, json_file, indent=4)
+    update_condition = f"userid = '{user_id}'"
+    update_data = {'aimode': True}
+    db_handler.update_data('users', update_data, update_condition)
 
 def updateLastAImsgTime(user_id):
-    """Update user's lastAImsgTime in userInfo.json to the current time."""
+    """Update the user's lastAImsgTime to the current time."""
 
-    # Read the JSON file
-    with open(json_file_path, 'r') as json_file:
-
-        #Load JSON data
-        data = json.load(json_file)
-
-        # Search the user info and update his/her AImode into active(true).
-        for user in data:
-            if user.get("userId") == user_id:
-                user.update({"lastAImsgTime": time.time()})
-                break
-
-    # Write the file
-    with open(json_file_path, 'w') as json_file:
-        json.dump(data, json_file, indent=4)
+    update_condition = f"userid = '{user_id}'"
+    update_data = {'lastaimsgtime': time.time()}
+    db_handler.update_data('users', update_data, update_condition)
 
 def askChatPDF(user_message):
     """Call chatPDF API to ask questions."""
@@ -235,31 +195,33 @@ def askChatPDF(user_message):
     
     return response_msg
 
-def check_idle_users(exit_event):
+def check_idle_user(exit_event):
     """Periodically check idle users and sends notifications if their idle time exceeds 5 mins"""
+    # Don't know why this while loop was executed twice every 5 sec,
+    # so just put a counter to ensure it only execute once every 5 sec.
 
     # Periodically check idle users and send notifications
     while not exit_event.is_set():
-        with file_lock:
-            # Check if there are any scheduled tasks that need to be executed
-            schedule.run_pending()
 
-            with open(json_file_path, 'r') as json_file:
+        # Check if there are any scheduled tasks that need to be executed
+        schedule.run_pending()
+        current_time = time.time()
+        print(current_time)
+        users = db_handler.select_data('users', columns=['username', 'userid', 'aimode', 'lastaimsgtime'])
+        
+        for user in users:
+            userId = user[1]
+            userLastAImsgTime = user[3]
 
-                # Load JSON data
-                data = json.load(json_file)
+            # Only deactivate AI-mode and sends notification if the user is in AI-mode.
+            if checkUserModeStatus(userId):
 
-                currunt_time = time.time()
-                for user in data:
+                # Calculate user's idle time, and see if the user's idle time exceed 5 mins.
+                idle_time = current_time - userLastAImsgTime
+                if idle_time > 60:
+                    exitAImodeNotification(userId)
+                    exitAImode(userId)
 
-                    # Only deactivates AI mode and sends notification if the user is in AI mode
-                    if checkUserModeStatus(user['userId']):
-
-                        # Calculate user's idle time, and see if the user's idle time exceed 5 mins.
-                        idle_time = currunt_time - user['lastAImsgTime']
-                        if idle_time > 60:
-                            exitAImodeNotification(user['userId'])
-                            exitAImode(user['userId'])
         time.sleep(5)
 
 def exitAImodeNotification(user_id):
@@ -272,46 +234,26 @@ def exitAImodeNotification(user_id):
     line_bot_api.push_message(user_id, messages=notificationMsg)
 
 def exitAImode(user_id):
-    """Turn user's AImode status into deactive(false)."""
+    """Turn user's AImode status into passive(false)."""
 
-    # Read the JSON file
-    with open(json_file_path, 'r') as json_file:
-
-        # Load JSON data
-        data = json.load(json_file)
-
-        # Search the user info and update his/her AImode into deactive(false).
-        for user in data:
-            if user.get("userId") == user_id:
-                user.update({"AImode": False})
-                break
-
-    # Write the file
-    with open(json_file_path, 'w') as json_file:
-        json.dump(data, json_file, indent=4)
+    update_condition = f"userid = '{user_id}'"
+    update_data = {'aimode': False}
+    db_handler.update_data('users', update_data, update_condition)
 
 def reset_status():
     """Load the userInfo.json, reset the "quota" to 50 of every user, and save it back."""
-    
-    with open(json_file_path, 'r') as json_file:
-        data = json.load(json_file)
 
-        # Reset every users' quota of messages to 50
-        for user in data:
-            user['quota'] = 50
-
-    with open(json_file_path, 'w') as json_file:
-        json.dump(data, json_file, indent=4)
+    update_condition = "True"
+    update_data = {"quota": 50}
+    db_handler.update_data('users', update_data, update_condition)
 
 def scheduled_reset(exit_event):
     """Reset users' quota to 50 upon everyday midnight """
 
-
     while not exit_event.is_set():
-        with file_lock:
-            # Check if there are any scheduled tasks that need to be executed
-            schedule.run_pending()
-            schedule.every().day.at("00:00").do(reset_status)
+        # Check if there are any scheduled tasks that need to be executed
+        schedule.run_pending()
+        schedule.every().day.at("00:00").do(reset_status)
 
         # Introduces a small delay of 1 sec between iterations of the loop, 
         # preventing the loop from consuming excessive CPU resources
@@ -319,30 +261,36 @@ def scheduled_reset(exit_event):
 
 def main():
     """Ran as a background task and continuously check the time
-     to check if resetting users' quota of message is needed."""
+     to check if resetting users' quota of message is needed.
+     Also continuously checking users' idle time and deactivate AImode when meeded"""
     
+    lock = threading.Lock()
+
     # Create an Event to  signal the thread to exit.(Upon Ctrl+c is pressed)
     exit_event = threading.Event()
-
-    # Start a thread for the scheduled task
-    schedule_thread = threading.Thread(target=scheduled_reset, args=(exit_event,))
-    schedule_thread.start()
-
-    # Start a thread for consistently check users' idle time
-    check_idle_thread = threading.Thread(target=check_idle_users, args=(exit_event,))
-    check_idle_thread.start()
-
     try:
+        # Start a thread for the scheduled task
+        schedule_thread = threading.Thread(target=scheduled_reset, args=(exit_event,))
+        schedule_thread.start()
+
+        # Start a thread for consistently check users' idle time
+        check_idle_thread = threading.Thread(target=check_idle_user, args=(exit_event,))
+        check_idle_thread.start()
+
         # Run the Flask app
         app.run(debug=True)
 
     except KeyboardInterrupt:
-        # Ctrl+c is pressed, set the exit event and wait for the thread to exit
+        # Ctrl+c is pressed, set the exit event for threads to exit gracefully
         exit_event.set()
+
+        # Wait for the threads to exit
         schedule_thread.join()
         check_idle_thread.join()
+
+        # Close the database connection
+        db_handler.close_connection()
 
 if __name__ == "__main__":
 
     main()
-
---
+++ db_module/db_operations.py
@@ -0,0 +1,128 @@
+import time
+import psycopg2
+from psycopg2 import sql
+
+class PostgreSQLHandler:
+    def __init__(self, dbname, user, password, host, port):
+        self.connection = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+        )
+        self.cursor = self.connection.cursor()
+
+    def create_table(self, table_name, columns):
+        query = sql.SQL("CREATE TABLE IF NOT EXISTS {} ({})").format(
+            sql.Identifier(table_name),
+            sql.SQL(', ').join(
+                sql.SQL("{} {}").format(
+                    sql.Identifier(column_name),
+                    sql.SQL(column_type)
+                ) for column_name, column_type in columns.items()
+            )
+        )
+        self.execute_query(query)
+
+    def insert_data(self, table_name, data):
+        query = sql.SQL("INSERT INTO {} ({}) VALUES ({})").format(
+            sql.Identifier(table_name),
+            sql.SQL(', ').join(map(sql.Identifier, data.keys())),
+            sql.SQL(', ').join(map(sql.Literal, data.values()))
+        )
+        self.execute_query(query)
+
+    def select_data(self, table_name, columns=None, condition=None):
+        if columns:
+            query = sql.SQL("SELECT {} FROM {}").format(
+                sql.SQL(', ').join(map(sql.Identifier, columns)),
+                sql.Identifier(table_name)
+            )
+        else:
+            query = sql.SQL("SELECT * FROM {}").format(sql.Identifier(table_name))
+
+        if condition:
+            query += sql.SQL(" WHERE {}").format(sql.SQL(condition))
+
+        return self.execute_query(query, fetchall=True)
+
+    def update_data(self, table_name, update_data, condition):
+        query = sql.SQL("UPDATE {} SET {} WHERE {}").format(
+            sql.Identifier(table_name),
+            sql.SQL(', ').join(
+                sql.SQL("{} = {}").format(
+                    sql.Identifier(column),
+                    sql.Literal(value)
+                ) for column, value in update_data.items()
+            ),
+            sql.SQL(condition)
+        )
+        self.execute_query(query)
+
+    def delete_data(self, table_name, condition):
+        query = sql.SQL("DELETE FROM {} WHERE {}").format(
+            sql.Identifier(table_name),
+            sql.SQL(condition)
+        )
+        self.execute_query(query)
+
+    def execute_query(self, query, fetchall=False):
+        try:
+            self.cursor.execute(query)
+            if fetchall:
+                return self.cursor.fetchall()
+            else:
+                self.connection.commit()
+        except Exception as e:
+            self.connection.rollback()
+            print(f"Error: {e}")
+        # finally:
+        #     self.cursor.close()
+
+    def close_connection(self):
+        self.connection.close()
+
+
+# Example Usage:
+
+# Initialize the PostgreSQLHandler
+# db_handler = PostgreSQLHandler(
+#     dbname='linebot',
+#     user='postgres',
+#     password='1234',
+#     host='localhost',
+#     port='5432'
+# )
+
+# # Insert data
+# user_id = 5678
+# user_name = "Joe"
+# data_to_insert = {'userid': user_id, 'username': user_name, 'quota': 30}
+# db_handler.insert_data('users', data_to_insert)
+
+# # Select data
+# selected_data = db_handler.select_data('users', condition=f"username = '{user_name}'")
+# print("Selected Data:", selected_data)
+# print("userName: ", selected_data[0][1])
+
+# #Update data
+# user_id = "Test1234"
+# user_name = "Test5678"
+# update_condition = "True"
+# update_data = {"quota": 50}
+# db_handler.update_data('users', update_data, update_condition)
+
+
+# # Select updated data
+# user_data = db_handler.select_data('users', columns=['username', 'userid', 'aimode', 'lastaimsgtime'])
+# print("User Data:", user_data)
+# for user in user_data:
+#     print("user id: ", user[1])
+
+# # Delete data
+# delete_condition = "id = 1"
+# db_handler.delete_data('example_table', delete_condition)
+
+# Close the connection
+# db_handler.close_connection()
